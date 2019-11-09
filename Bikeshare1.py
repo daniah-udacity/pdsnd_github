327c9915284f33b21b6b073fd1235d655d41fb31
Daniah <t.daniah1@gmail.com>
2019-11-09 14:48:49 +0300

    Add more comments to bikeshare.py

--- Bikeshare1.py
+++ Bikeshare1.py
@@ -59,6 +59,15 @@ def get_filters():
     return city_name,month,day_of_week
 
 def load_data(city,month,day):
+	 """
+    Loads data for the specified city and filters by month and day if applicable.
+    Args:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    Returns:
+        df - Pandas DataFrame containing city data filtered by month and day
+    """
    
     df = pd.read_csv(CITY_DATA[city])
     """ To load the selected city csv file """
@@ -78,6 +87,7 @@ def load_data(city,month,day):
     return df
 
 def time_stats(df):
+	"""Displays statistics on the most frequent times of travel."""
 
     print("Calculating the Most Frequent Time's of travel")
     start_time = time.time()
